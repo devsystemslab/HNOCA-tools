cccd7b6ee59e1277c1317b75923e0216dd4abdca
Marius Lange <marius.lange@bsse.ethz.ch>
2025-03-19 17:21:31 +0100

    Add the mapping module

--- src/hnoca/__init__.py
+++ src/hnoca/__init__.py
@@ -2,6 +2,6 @@ from importlib.metadata import version
 
 from . import snapseed, stats, utils
 
-__all__ = ["snapseed", "stats", "utils"]
+__all__ = ["snapseed", "stats", "utils", "map"]
 
 __version__ = version("hnoca")
