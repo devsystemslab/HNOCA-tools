878170f6dc2505ebce830aa8052c1708d321b325
Marius Lange <marius.lange@bsse.ethz.ch>
2025-03-22 15:48:16 +0100

    Complete first round of basic annotation tests

--- tests/snapseed/test_annotate.py
+++ tests/snapseed/test_annotate.py
@@ -38,29 +38,73 @@ def test_annotate_invalid_depth(request, adata_annotate, marker_fixture):
         annotate(adata_annotate, markers, group_name="leiden")
 
 
-def test_annotate_h1(adata_annotate, marker_h1):
+def test_annotate(adata_annotate, marker_h1):
     """
     Test that annotate works with a non-hierarchical marker dict.
     """
     # Generate annotations with snapseed
     ann_df = annotate(adata_annotate, marker_h1, group_name="leiden")
     mapping = ann_df.set_index("leiden")["class"].to_dict()
-    adata_annotate.obs["snapseed_class"] = adata_annotate.obs["leiden"].map(mapping)
+    adata_annotate.obs["snap_class"] = adata_annotate.obs["leiden"].map(mapping)
 
     # Get the actual counts
-    actual_counts = adata_annotate.obs.groupby("snapseed_class", observed=True).size()
+    actual_counts = adata_annotate.obs.groupby("snap_class", observed=True).size()
     actual_counts.index = actual_counts.index.astype(object)
 
     # Create the expected pandas Series
     expected_counts = pd.Series(
-        [348, 713, 141, 1498], index=["B_cell", "Monocyte", "NK_cell", "T_cell"], name="snapseed_class"
+        [348, 713, 141, 1498], index=["B_cell", "Monocyte", "NK_cell", "T_cell"], name="snap_class"
     )
 
     # Compare the to Series
     pd.testing.assert_series_equal(
         actual_counts.sort_index(),
         expected_counts.sort_index(),
-        obj="snapseed_class counts",
+        obj="snap_class counts",
+        check_index_type=False,
+        check_names=False,
+    )
+
+
+def test_annotate_hierachy(adata_annotate, marker_h2):
+    """
+    Test that annotate_hierarchy works with a hierarchical marker dict.
+    """
+    # Generate annotations with snapseed
+    ann_dict = annotate_hierarchy(adata_annotate, marker_h2, group_name="leiden")
+    ann_df = ann_dict["assignments"]
+
+    # map to AnnData
+    for key in ann_df:
+        mapping = ann_df[key].to_dict()
+        adata_annotate.obs[f"snap_{key}"] = adata_annotate.obs["leiden"].map(mapping)
+
+    # Validate level 1 annotation counts
+    actual_level1 = adata_annotate.obs.groupby("snap_level_1", observed=True).size()
+    actual_level1.index = actual_level1.index.astype(object)
+    expected_level1 = pd.Series(
+        [348, 713, 141, 1498], index=["B_cell", "Monocyte", "NK_cell", "T_cell"], name="snap_level_1"
+    )
+    pd.testing.assert_series_equal(
+        actual_level1.sort_index(),
+        expected_level1.sort_index(),
+        obj="snap_level_1 counts",
+        check_index_type=False,
+        check_names=False,
+    )
+
+    # Validate level 2 annotation counts
+    actual_level2 = adata_annotate.obs.groupby("snap_level_2", observed=True).size()
+    actual_level2.index = actual_level2.index.astype(object)
+    expected_level2 = pd.Series(
+        [1322, 176, 542, 171],
+        index=["CD4_T_cell", "CD8_T_cell", "Classical_monocyte", "Non_classical_monocyte"],
+        name="snap_level_2",
+    )
+    pd.testing.assert_series_equal(
+        actual_level2.sort_index(),
+        expected_level2.sort_index(),
+        obj="snap_level_2 counts",
         check_index_type=False,
         check_names=False,
     )
